fc17155038f540be766b8437c097837a623579d8
Anton (Atilla) Tsigularov <antont@opera.com>
2012-07-26 13:07:14 +0200

    Cleaning up the launch script so it's not such a mess

--- launch_thumbsup.py
+++ launch_thumbsup.py
@@ -12,33 +12,35 @@ import thumbsup.paths
 from settings import settings
 
 
-subfolders = thumbsup.paths.get_subs(settings['static_path'])
-if len(subfolders) < 2:
-    ThumbsHandler = partial(ThumbnailHandler, settings=settings)
-else:
-    logging.debug("Subfolders found at static_path, using advanced digest")
-    digest = thumbsup.paths.consistent_two_level(subfolders)
-    ThumbsHandler = partial(ThumbnailHandler, settings=settings,
-                            digest=digest)
+def init_application():
+    subfolders = thumbsup.paths.get_subs(settings['static_path'])
+    if len(subfolders) < 2:
+        ThumbsHandler = partial(ThumbnailHandler, settings=settings)
+    else:
+        logging.debug("Subfolders found at static_path, using advanced digest")
+        digest = thumbsup.paths.consistent_two_level(subfolders)
+        ThumbsHandler = partial(ThumbnailHandler, settings=settings,
+                                digest=digest)
 
-handlers = [
-    (r"/", ThumbsHandler),
-    (r"/static/(.*)", tornado.web.StaticFileHandler,
-     {"path": settings['static_path']}),
-]
+    handlers = [
+        (r"/", ThumbsHandler),
+        (r"/static/(.*)", tornado.web.StaticFileHandler,
+         {"path": settings['static_path']}),
+        ]
 
-application = tornado.web.Application(handlers,
-                                      static_path=settings["static_path"])
+    return tornado.web.Application(handlers,
+                                   static_path=settings["static_path"])
+
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-    rootLogger = logging.getLogger('')
-    rootLogger.setLevel(logging.DEBUG)
 
     try:
-        http_server = tornado.httpserver.HTTPServer(application,
+        http_server = tornado.httpserver.HTTPServer(init_application(),
                                                     xheaders=True)
         http_server.listen(settings["port"])
+
+        logging.info("Initialized, Starting server")
         tornado.ioloop.IOLoop.instance().start()
     except KeyboardInterrupt:
         logging.info("IOLoop terminated by user")
