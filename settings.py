35d36c46e4bd7679e57c39f8d5fc6cd3ae95751b
Anton (Atilla) Tsigularov <antont@opera.com>
2012-10-25 17:06:25 +0200

    Forgot to include the settings file, with image_format included.

--- settings.py
+++ settings.py
@@ -1,12 +1,14 @@
 import os
+
+IMG_FORMATS = ("png", "jpg", "gif", "svg")
+
 settings = {
     "port": 8888,
     "static_path": "/tmp",
     "phantomjs_path": "phantomjs",
-    "render_script": os.getcwd()+"/render_one.coffee",
+    "render_script": os.getcwd() + "/render_one.coffee",
     "view_size": "1280x1024",
     "thumb_size": "320x200",
     "ua_string": "Thumbnail Bot 1.0",
+    "image_format": "jpg",
 }
-
-
